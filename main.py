bace7efdd53801c270c055179f5b5eba4b7d8961
Ahmed Aziz Smaoui <42848675+loading04@users.noreply.github.com>
2023-04-26 17:46:44 +0100

    add fetching from api feature

--- main.py
+++ main.py
@@ -8,14 +8,15 @@ list_post_obj = []
 
 post_json = requests.get("https://api.npoint.io/ca01e4f19c5ba9f73f70").json()
 
-for post in post_json:
-    post_obj = Post(post_id=post['id'], title=post['title'], subtitle=post['subtitle'], body=post['body'])
-    list_post_obj.append(post_obj)
+#for post in post_json:
+#    post_obj = Post(post_id=post['id'], title=post['title'], subtitle=post['subtitle'], body=post['body'])
+#    list_post_obj.append(post_obj)
 
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("index.html", list=post_json)
+
 
 @app.route('/about')
 def about():
@@ -27,17 +28,15 @@ def contact():
     return render_template("contact.html")
 
 
-
 @app.route("/post/<int:index>")
 def show_post(index):
     requested_post = None
     for blog_post in list_post_obj:
-        if int(blog_post.id) == index :
+        if int(blog_post.id) == index:
             requested_post = blog_post
-            return render_template("post.html", post=requested_post)
+            return render_template("post.html", post=post_json)
 
     return "<h1>404 not found</h1>"
-
 
 
 if __name__ == "__main__":
