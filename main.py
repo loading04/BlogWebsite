bebfabaeee4ffed46fbb0246f356f297811fccad
Ahmed Aziz Smaoui <42848675+loading04@users.noreply.github.com>
2023-04-26 17:18:31 +0100

    fixing routing between pages

--- main.py
+++ main.py
@@ -17,6 +17,16 @@ for post in post_json:
 def home():
     return render_template("index.html")
 
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+
 
 @app.route("/post/<int:index>")
 def show_post(index):
